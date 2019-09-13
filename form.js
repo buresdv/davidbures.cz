01df3ce080a6b9576bd48e62c5412ed050ec5d8a
David Bureš <buresd@gmail.com>
2019-09-13 20:10:01 +0200

    Začátek práce na přihlašovacím modálu

---
+++ form.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(document).on('change', '#sluzby', function(e) {
+        vybranaSluzba = this.options[e.target.selectedIndex].text;
+        console.log(vybranaSluzba);
+        
+        if (vybranaSluzba == 'Překlad') {
+            $('vybranPreklad').addClass('vybrano');
+            $('vybranaKorektura').removeClass('vybrano');
+            $('vybranoTlumoceni').removeClass('vybrano');
+        } else if (vybranaSluzba == 'Korektura') {
+            $('vybranaKorektura').addClass('vybrano');
+            $('vybranPreklad').removeClass('vybrano');
+            $('vybranoTlumoceni').removeClass('vybrano');
+        } else if (vybranaSluzba == 'Tlumočení') {
+            $('vybranoTlumoceni').addClass('vybrano');
+            $('vybranPreklad').removeClass('vybrano');
+            $('vybranaKorektura').removeClass('vybrano');
+        }
+
+    });
+});
