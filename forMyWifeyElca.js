1b39cd5e9fce76270995b614862c955209a7ef4e
David Bureš <buresdv@gmail.com>
2020-05-12 21:49:38 +0200

    Elča is the best wifeeee of all timeee and FOREVER

---
+++ forMyWifeyElca.js
@@ -0,0 +1,6 @@
+function justForTheBestGirlAndTheOnlygirlInMyLifeeee(e) {
+    var evtobj = window.event? event : e
+    if (evtobj.keyCode == 69 && evtobj.altKey) alert("I love you, my perfect wife Elča ❤");
+}
+
+document.onkeydown = justForTheBestGirlAndTheOnlygirlInMyLifeeee;
